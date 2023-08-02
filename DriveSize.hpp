872d64f0e0b730932788082beb2a3be4a7e8a091
RiseGhost <jomiguelsa2mt@gmail.com>
2023-08-02 03:22:22 +0100

    update

--- DriveSize.hpp
+++ DriveSize.hpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+//Este ficheiro dedica-se a implementação de código para a obtenção de dados
+//sobre as unidades de armazenamento
+
+//Retorna um vetor com todas as unidades de armazenamento que estão disponiveis:
 vector<string> GetAvailableDrives(){
     vector<string> drives;
     DWORD drivesMash = GetLogicalDrives();
